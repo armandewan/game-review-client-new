6094fb35c96d969df8aae6b703f35d5db8ab4e07
armandewan <mdarmandewan88@gmail.com>
2024-12-09 00:00:27 +0600

    Update Layouts.jsx

--- src/Layouts.jsx
+++ src/Layouts.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import Navbar from './Components/Navbar';
-import { Outlet } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import Footer from './Components/Footer';
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Footer from "./Components/Footer";
 
 const Layouts = () => {
-    return (
-        <div>
-            <nav className='bg-fuchsia-700 '>
-                <Navbar/>
-            </nav>
-            <main>
-                <Outlet/>
-            </main>
+  const [color, setColor] = useState(true);
+  return (
+    <div className={`${color=== true ? 'bg-white text-black' : 'bg-gray-400 text-blue-700'}`}>
+      <div className="flex justify-end gap-2 p-2">
+        <span>Dark</span>
+        <input type="checkbox" className="toggle" defaultChecked onChange={()=>setColor(!color)}/>
+        <span>light</span>
+      </div>
+      <nav className="bg-fuchsia-700 ">
+        <Navbar />
+      </nav>
+      <main>
+        <Outlet />
+      </main>
 
-            <footer>
-                <Footer/>
-            </footer>
-            <ToastContainer />
-        </div>
-    );
+      <footer>
+        <Footer />
+      </footer>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default Layouts;
